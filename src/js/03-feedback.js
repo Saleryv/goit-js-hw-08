250baa8e9a6e596cb0da84f5b1e8f2a601b32823
Saleryv <seryoginvaleriy@gmail.com>
2022-11-18 16:10:52 +0200

    Task 3

--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -1,1 +1,45 @@
+import  throttle from 'lodash.throttle'; 
 
+
+const LOCALSTORAGE_KEY = 'feedback-form-state';
+
+const refs = {
+    form: document.querySelector('.feedback-form'),
+    input: document.querySelector('.feedback-form input'),
+    textarea: document.querySelector('.feedback-form textarea'),
+}
+const formData = {};
+
+
+refs.form.addEventListener('submit', onFormSubmit);
+refs.textarea.addEventListener('input', throttle(onTextareaInput, 500));
+refs.form.addEventListener('input', event => {
+formData[event.target.name] = event.target.value;
+});
+
+populateTextarea();
+
+
+function onFormSubmit(event) {
+    event.preventDefault();
+    event.target.reset();
+    localStorage.removeItem(LOCALSTORAGE_KEY);
+    console.log(JSON.stringify(formData));
+}
+
+
+function onTextareaInput() {
+    const message = JSON.stringify(formData);
+    localStorage.setItem(LOCALSTORAGE_KEY, message);
+}
+
+
+
+
+function populateTextarea() {
+    const savedMessage = localStorage.getItem(LOCALSTORAGE_KEY);
+
+    if (savedMessage) {
+        refs.textarea.value = savedMessage;
+    }
+}
